77c573d61892d638f9dedf75c225dfbcf42a024e
gengming <gengming@chinalife.com.hk>
2018-06-08 19:38:48 +0800

    add register function and login/logout success!

--- jobplus/handlers/front.py
+++ jobplus/handlers/front.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, render_template
-from flask import redirect, url_for
+from flask import Blueprint, render_template, flash, redirect, url_for, request, current_app
 from flask_login import login_user, logout_user, login_required
 
 from jobplus.models import User
-from jobplus.forms import LoginForm
+from jobplus.forms import LoginForm, CompanyregisterForm
+
 
 front = Blueprint('front', __name__)
 
@@ -23,3 +23,20 @@ def login():
 
     return render_template('login.html', form=form)
 
+
+@front.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You are logout!', 'success')
+    return redirect(url_for('.index'))
+
+
+@front.route('/companyregister', methods=['GET', 'POST'])
+def companyregister():
+    form = CompanyregisterForm()
+    if form.validate_on_submit():
+        form.create_user()
+        flash('Company Register Success, please login!', 'success')
+        return redirect(url_for('.login'))
+    return render_template('/company/register.html', form=form)
